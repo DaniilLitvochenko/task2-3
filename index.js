6ea0b0b8071d059053e602613efabb27b8dadb1e
DaniilLitovchenko <danay2000@inbox.ru>
2019-09-28 12:32:46 +0300

    Первый коммит

---
+++ index.js
@@ -0,0 +1,11 @@
+/** @const {string} */ const hiWord='Hello';
+/**
+*
+*
+*/
+
+function sayHello(name='Nameless'){
+  return `${hiWord}, ${name}!`;
+}
+console.log(sayHello());
+console.log(sayHello());
